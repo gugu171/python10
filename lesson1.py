c0bc4635c76ce874f19e826c11424d9f3cf6ba98
gugu171 <168572259+gugu171@users.noreply.github.com>
2026-03-23 16:39:54 +0000

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,11 @@
+print("My name is Uvan")
+print("I am 10 years old")
+print("My favourite sport is badminton \nMy favourite subject is Computing")
+w=35
+print("My weight is",w,"kg")
+print("The sum of 25 and 17 = ",25+17) #sum
+r="Uvan"
+n=10
+p="United Kingdom"
+print("My name is",r,"I am ",n,"years old. I live in the ",p)
+
