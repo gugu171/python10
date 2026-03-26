80624ef3ec15c5601300ed0ced4dd17e8369bee6
gugu171 <168572259+gugu171@users.noreply.github.com>
2026-03-26 17:22:37 +0000

    Add files via upload

---
+++ kw.py
@@ -0,0 +1,3 @@
+import keyword
+print("Keywords in python: ")
+print(keyword.kwlist)
---
+++ userinput.py
@@ -0,0 +1,17 @@
+name=input("Enter your name ")
+age=int(input("Enter your age"))
+sub=input("Your favourite subject ")
+game=input("Your favourite game ")
+
+print("My name is",name)
+print("My age is",age)
+print("My favourite subject is",sub)
+print("My favourite game is",game)
+
+#Arithmetic operations
+a=int(input("Enter num1 = "))
+b=int(input("Enter num2 = "))
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
