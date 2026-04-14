a810107eabd70577d4454471e7e61c0c09d5a1f3
gugu171 <168572259+gugu171@users.noreply.github.com>
2026-04-14 16:16:15 +0100

    Add files via upload

---
+++ lighttt.py
@@ -0,0 +1,6 @@
+hot=int(input("Enter how many degrees it is right now: "))
+cold=5
+if hot>cold:
+    print("You can wear light clothes. It is too hot")
+else:
+    print("You must wear thick clothes. You might get a cold if you don't")
