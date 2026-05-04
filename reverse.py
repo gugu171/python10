21a958f3505762c42bc1ea085dde053ae5bbe6f5
gugu171 <168572259+gugu171@users.noreply.github.com>
2026-05-04 17:30:38 +0100

    Add files via upload

---
+++ reverse.py
@@ -0,0 +1,7 @@
+s=input("Enter a word : ")
+s2=""
+for i in s:
+    s2=i+s2
+print("The word in reverse is:",s2)
+
+print(s[::-1])
---
+++ reverseorder.py
@@ -0,0 +1,6 @@
+n=int(input("Enter a number: "))
+for i in range(1,n+1):
+    print(i)
+print("The reversed order is:")
+for i in range(n,0,-1):
+    print(i)
