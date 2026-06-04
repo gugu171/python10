765fc26dc6cf44f4f2fd69561a8cb1e3abb22ef4
gugu171 <168572259+gugu171@users.noreply.github.com>
2026-06-04 17:52:14 +0100

    Add files via upload

---
+++ secretnumber.py
@@ -0,0 +1,11 @@
+print("Welcome to the number guessing game!")
+number = 29
+guess = int(input("Enter your guess! : "))
+lives = 5
+if guess == 29:
+    print("You got the answer!")
+else:
+    while lives  
+        print("Uh oh! You got it wrong :(. You have",i,"lives left.")
+    else:
+        print("Oh no! You got 0 lives left :( Maybe next time")
