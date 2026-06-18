3bcd0085e53313a4dca3d7423b2752a7a366f1a3
gugu171 <168572259+gugu171@users.noreply.github.com>
2026-06-18 17:12:18 +0100

    Add files via upload

---
+++ finda.py
@@ -0,0 +1,10 @@
+a = input("Enter a word : ")
+for i in a:
+    if (i == 'A'):
+        print("The letter 'A' has been found")
+        break
+    elif (i == 'a'):
+        print("The letter 'a' has been found")
+        break
+    else:
+        print("The letter A has not been found")
