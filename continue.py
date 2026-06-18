ff373754efa120ef19d3c848abaed38aa01f5afe
gugu171 <168572259+gugu171@users.noreply.github.com>
2026-06-18 17:24:20 +0100

    Update continue.py

--- continue.py
+++ continue.py
@@ -3,5 +3,5 @@ while var > 0:
     var = var - 1
     if var == 5:
         continue
-    print("KORRENT VARIBLE VALU =",var)
-print("NO MOR VARIBLE NAW. GUDBAI!")
+    print("\nKORRENT VARIBLE VALU =",var)
+print("\nNO MOR VARIBLE NAW. GUDBAI!")
