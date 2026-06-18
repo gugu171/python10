841cc837ec88f4be52deaf05ef5d29dac4e93704
gugu171 <168572259+gugu171@users.noreply.github.com>
2026-06-18 17:18:22 +0100

    Add files via upload

---
+++ pass.py
@@ -0,0 +1,11 @@
+for x in range(10):
+    if x % 20 == 0:
+        print("BOP IT!")
+    elif x % 15 == 0:
+        pass
+    elif x % 5 == 0:
+        print("TWIST IT!")
+    elif x % 3 == 0:
+        print("PULL IT!")
+    else:
+        print(x,"STEER IT")
