f6ce3851d0f50b8acbfb19d0f804f18d32187d29
gugu171 <168572259+gugu171@users.noreply.github.com>
2026-06-22 17:27:58 +0100

    Add files via upload

---
+++ bye.py
@@ -0,0 +1,9 @@
+valid = False
+while not valid:
+    try:
+       n=int(input("Enter a number : "))
+       while n % 2 == 0:
+           print("BYE HAHAAHAHAH YOU KAN NEVER STOP MEEEEEEE") 
+           vaild = True
+    except ValueError:
+        print("YU HAV ROTE THE RONG TING I AM KOMIN FOR YUUUUU")
