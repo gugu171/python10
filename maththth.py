cbe7a46054d331e7f1685aa6788cfaf04b209021
gugu171 <168572259+gugu171@users.noreply.github.com>
2026-07-02 16:39:41 +0100

    Add files via upload

---
+++ maththth.py
@@ -0,0 +1,12 @@
+import math
+
+angle = 0.5
+
+resultsin = math.sin(angle)
+print("sin is = to :",resultsin)
+
+resultcos = math.cos(angle)
+print("cos is = to :",resultcos)
+
+resulttan = math.tan(angle)
+print("tan is = to :",resulttan)
