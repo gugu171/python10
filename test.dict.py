2373a6e18ce57a5b78f04a06aaecc40cde9cb1bc
gugu171 <168572259+gugu171@users.noreply.github.com>
2026-07-09 17:39:43 +0100

    Add files via upload

---
+++ test.dict.py
@@ -0,0 +1,11 @@
+test_dict= {'codingal': 2, 'is': 2, 'best': 2, 'for': 2, 'coding': 1}
+
+print("The original dictionary is : "+ str(test_dict))
+
+K = 2
+res = 0
+for key in test_dict:
+    if test_dict[key] == K:
+        res = res + 1
+
+print("The frequency of K is: " + str(res))
