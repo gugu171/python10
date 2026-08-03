edd8654be1868a2e2eb7547121fc6d878f07d2d4
gugu171 <168572259+gugu171@users.noreply.github.com>
2026-08-03 17:31:14 +0100

    Add files via upload

---
+++ fm.py
@@ -0,0 +1,28 @@
+class FamilyMember():
+    def __init__(self, eye_color, height_cm):
+        self.eye_color = eye_color
+        self.height_cm = height_cm
+    
+    def show_traits(self):
+        print("Eye color:",self.eye_color)
+        print("Height in cm",self.height_cm)
+
+class Kid(FamilyMember):
+    def __init__(self, name, age, eye_color, height_cm):
+        self.name = name
+        self.age = age
+        super().__init__(eye_color, height_cm)
+
+    def show_traits(self):
+        print("Name:",self.name)
+        print("Age:",self.age)
+        super().show_traits
+
+    def favourite_hobby(self, hobby):
+        print(self.name,'loves',hobby)
+
+child = Kid("Dünkelskog", 345000, 'bäb', 7945)
+
+child.show_traits()
+child.favourite_hobby('eating')
+print("Hmm is dis kid a subclass of FamilyMember?",issubclass(Kid,FamilyMember))
