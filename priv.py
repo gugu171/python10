89cae125151cab291b103a881a0f75cf79e65e01
gugu171 <168572259+gugu171@users.noreply.github.com>
2026-08-13 17:10:37 +0100

    Add files via upload

---
+++ priv.py
@@ -0,0 +1,15 @@
+class mySuperDooperTrooperclass:
+
+    __privatevar = 27;
+
+    def __privateMeth(self):
+        print("hi im stuck inside mySuperDooperTrooperclass pls help mi here to much raining *gurglegurgle*")
+
+    def hello(self):
+        print("idk what private variable but someone told me the value so its",mySuperDooperTrooperclass.__privatevar)
+
+
+foo = mySuperDooperTrooperclass()
+foo.hello()
+print("guys beware a humongous error will come WATCH OUT")
+foo.__privateMeth
