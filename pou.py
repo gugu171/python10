5bc85a6f180ec62d361d3f871c37260d30a43b9b
gugu171 <168572259+gugu171@users.noreply.github.com>
2026-08-13 17:23:41 +0100

    Add files via upload

---
+++ pou.py
@@ -0,0 +1,10 @@
+class LEPOINT:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return '({0}, {1})'.format(self.x, self.y)
+
+p1 = LEPOINT(2, 3)
+print(p1)
